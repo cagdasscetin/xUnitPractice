4a21d3963bfb34455e60e8b6a40d7c8b4a13dec3
cagdasscetin <cagdasscetin@gmail.com>
2024-11-26 00:32:08 +0300

    Asserting Exceptions

--- xUnitPractice.Tests/CustomerTest.cs
+++ xUnitPractice.Tests/CustomerTest.cs
@@ -5,17 +5,17 @@ namespace xUnitPractice.Tests;
 public class CustomerTest
 {
     [Fact]
-    public void CheckNameNotEmpty()
-    {
-        var customer = new Customer();
-        Assert.NotNull(customer.Name);
-        Assert.False(string.IsNullOrEmpty(customer.Name));
-    }
-
-    [Fact]
     public void CheckLegitForDiscount()
     {
         var customer = new Customer();
         Assert.InRange(customer.Age, 15, 24);
+    }
+
+    [Fact]
+    public void GetOrdersByNameNotNull()
+    {
+        var customer = new Customer();
+        var exceptionDetails = Assert.Throws<ArgumentException>(() => customer.GetOrdersByName(null));
+        Assert.Equal("Hello", exceptionDetails.Message);
     }
 }
--- xUnitPractice/Customer.cs
+++ xUnitPractice/Customer.cs
@@ -2,6 +2,15 @@ namespace xUnitPractice;
 
 public class Customer
 {
-    public string Name => "Cagdas";
+    public int GetOrdersByName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentException("Hello");
+        }
+
+        return 100;
+    }
+    
     public int Age => 24;
 }
